011e7f9827c69079a7ca90aee59b87436e19fe85
Ulises Montenegro <ulimontenegro23@gmail.com>
2023-07-07 20:52:39 -0300

    first draft of the Tienda section

--- next.config.js
+++ next.config.js
@@ -1,4 +1,37 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'i.ibb.co',
+          port: '',
+          pathname: '/0tjx9d8/dyj-titular.webp',
+        },
+        {
+          protocol: 'https',
+          hostname: 'i.ibb.co',
+          port: '',
+          pathname: '/fD878ss/dyj-suplente.webp',
+        },
+        {
+          protocol: 'https',
+          hostname: 'i.ibb.co',
+          port: '',
+          pathname: '/hgsJkws/dyj-arquero.webp',
+        },
+        {
+          protocol: 'https',
+          hostname: 'i.ibb.co',
+          port: '',
+          pathname: '/NWnrNww/dyj-alternativa.webp',
+        },
+        {
+          protocol: 'https',
+          hostname: 'i.ibb.co',
+          port: '',
+          pathname: '/pK9Rv5F/dyj-entrenamiento.webp',
+        },
+      ],
+    },
+  }
--- src/app/(pages)/tienda/page.tsx
+++ src/app/(pages)/tienda/page.tsx
@@ -1,7 +1,18 @@
-import InConstruction from "@/components/constructionPage/InConstruction";
+import Producto from './Producto'
+import styles from './tienda.module.css'
+import data from '@/data.json'
 
 export default function Tienda () {
     return(
-        <InConstruction />
+        <main className={styles.tienda}>
+            <div className={styles.tienda__hero}>
+                <h1 className={styles.tienda__hero__title}>Indumentaria del Halcón</h1>
+            </div>
+            <section className={styles.tienda__container}>
+                { data.map(producto => {
+                    return <Producto key={producto.id} producto={producto} />
+                }) }
+            </section>
+        </main>
     )
 }
